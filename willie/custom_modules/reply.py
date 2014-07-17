eaaf7afdaedd46490b70ff0045373ced3333976b
reveille <mikerev@gmail.com>
2014-07-17 10:25:22 -0500

    revtest

--- willie/custom_modules/reply.py
+++ willie/custom_modules/reply.py
@@ -3,3 +3,7 @@ from willie import module
 @module.rule('hello!?')
 def hi(bot, trigger):
    bot.say('Hi, ' + trigger.nick)
+
+@module.rule('does anyone know')
+def notgoogle(bot, trigger):
+  bot.reply('Does this look like Google to you?') 
