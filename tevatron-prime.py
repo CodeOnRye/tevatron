c6177c058efdc83f828aaf49c11d9475d106182b
reveille <mikerev@gmail.com>
2014-08-12 18:54:24 -0500

    boobs

--- tevatron-prime.py
+++ tevatron-prime.py
@@ -2,5 +2,7 @@
 import subprocess
 
 peak = ['ln', '-s', '/home/tevatron/.willie/peak.db', '/home/tevatron/tevatron/willie/custom_modules/peak.db']
+karma = ['ln', '-s', '/home/tevatron/.willie/karma.db', '/home/tevatron/tevatron/willie/custom_modules/karma.db']
 subprocess.Popen(peak)
+subprocess.Popen(karma)
 
