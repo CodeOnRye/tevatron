61345c68d85a34284fef2a5a287d864373a0be25
reveille <mikerev@gmail.com>
2014-09-27 10:51:12 -0500

    AI improvements

--- tevatron-prime.py
+++ tevatron-prime.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 import subprocess
+from sys import argv
+
+script, job = argv
 
 peak = ['ln', '-s', '/home/tevatron/.willie/peak.db', '/home/tevatron/tevatron/willie/custom_modules/peak.db']
 karma = ['ln', '-s', '/home/tevatron/.willie/karma.db', '/home/tevatron/tevatron/willie/custom_modules/karma.db']
-subprocess.Popen(peak)
-subprocess.Popen(karma)
+quotes = ['ln', '-s', '/home/tevatron/.willie/quote.db', '/home/tevatron/tevatron/willie/custom_modules/quote.db']
 
+def SymLinks():
+    subprocess.Popen(peak)
+    subprocess.Popen(karma)
+    subprocess.Popen(quotes)
+
+if job == 'prime-db':
+  SymLinks()
+else:
+    print "You need an argument..."
--- willie/custom_modules/reply.py
+++ willie/custom_modules/reply.py
@@ -1,9 +1,39 @@
 from willie import module
 
-@module.rule('hello!?')
+@module.rule('hello?')
 def hi(bot, trigger):
-   bot.say('Hi, ' + trigger.nick)
+    bot.say('Hi, ' + trigger.nick)
 
 @module.rule('does anyone know')
 def notgoogle(bot, trigger):
-  bot.reply('Does this look like Google to you?') 
+    bot.reply('Does this look like Google to you?') 
+
+@module.rule('good morning everyone')
+def notgoogle(bot, trigger):
+    bot.say('Good morning, ' + trigger.nick)
+
+@module.rule('hi Tevatron')
+def notgoogle(bot, trigger):
+    bot.say('Heya, ' + trigger.nick)
+
+@module.rule('sup Tevatron')
+def notgoogle(bot, trigger):
+    bot.say('What\'it do, mang.')
+
+@module.rule('smoke weed')
+def notgoogle(bot, trigger):
+    bot.say('SMOKE WEEEEED ERRDAY.')
+
+@module.rule('Tevatron, what do you do?')
+def notgoogle(bot, trigger):
+    bot.say('ERRDAY I\'M SPINNIN')
+
+@module.rule('good morning, Tevatron')
+def notgoogle(bot, trigger):
+    bot.say('Good morning, ' + trigger.nick)
+
+@module.rule('good evening, Tevatron')
+def notgoogle(bot, trigger):
+    bot.say('Good evening, ' + trigger.nick)
+
+
