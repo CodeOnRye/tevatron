a922014bdff3a71a4d59ac91761bced4910ef880
reveille <mikerev@gmail.com>
2014-10-08 20:12:37 -0500

    stats.py script

---
+++ willie/custom_modules/stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# TO DO:
+#
+# * Tevatron's own results keep artificially incrementing the word count.
+#
+#
+from willie import *
+import codecs
+
+
+logfile = "/home/tevatron/.willie/logs/raw.log"
+epoch = "blah blah blah"
+
+@module.commands('stats')
+@module.example('.stats')
+def stats(bot, trigger):
+        word = trigger.group(2)
+        bot.reply('Scouring the archives for your request.....')
+	total = 0
+	with codecs.open(logfile, 'r', 'utf-8') as f:
+	    for line in f:
+	        finded = line.find(word)
+	        if finded != -1 and finded != 0:
+	            total += 1
+	results = "The word %s has been mentioned %s times since %s." % (word, total, epoch)
+	bot.reply(results)
+	
+
