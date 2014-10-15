4c70da50e4758e3e05fc59209703265a7671e1e2
reveille <mikerev@gmail.com>
2014-10-14 19:51:21 -0500

    demo re function for stats

--- willie/custom_modules/stats.py
+++ willie/custom_modules/stats.py
@@ -6,7 +6,7 @@
 #
 #
 from willie import *
-import codecs
+import codecs, re
 
 
 logfile = "/home/tevatron/.willie/logs/raw.log"
@@ -17,12 +17,10 @@ epoch = "Jan 29  2014"
 def stats(bot, trigger):
         word = trigger.group(2)
         bot.reply('Scouring the archives for your request, this may take a minute.....')
-	total = 0
 	with codecs.open(logfile, 'r', 'utf-8') as f:
-	    for line in f:
-	        finded = line.find(word)
-	        if finded != -1 and finded != 0:
-	            total += 1 
+            strings = re.findall(r'%s' % word, f.read())
+            total = len(strings)
+
 	results = "The word %s has been mentioned %s times since %s." % (word, total, epoch)
 	bot.reply(results)
 	
